c2cd5c1e358f1f29a011b6cb72ae0852069d0348
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-28 17:42:33 -0400

    Update lexer_test.c
    Fixed syntax error on Line 9!

--- src/lexer_test.c
+++ src/lexer_test.c
@@ -5,8 +5,8 @@ int main(void)
 {
     const char *input = "int main(void) { return 0; }";
     Token tok;
-    while ((tok = next_token(&input)).kind != TOKEN_EOF) {
-        printf("[%s, \"%s\"]\n", token_kind_to_string(tok.kind), tok.lexeme);
+    while ((tok = next_token(&input)).type != TOKEN_EOF) {
+        printf("[%s, \"%s\"]\n", token_kind_to_string(tok.type), tok.lexeme);
         free(tok.lexeme);
     }
     return 0;
