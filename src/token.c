8286199addc92dc24d24e26d70e3be4f0a4c4d9b
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-28 17:02:28 -0400

    Create token.c
    Built src/token.c to implement functionality of token.h

---
+++ src/token.c
@@ -0,0 +1,14 @@
+#include "token.h"
+
+const char *token_kind_to_string(TokenKind kind) {
+    switch (kind) {
+        case TOKEN_EOF:              return "TOKEN_EOF";
+        case TOKEN_KEYWORD:          return "TOKEN_KEYWORD";
+        case TOKEN_IDENTIFIER:       return "TOKEN_IDENTIFIER";
+        case TOKEN_INTEGER_LITERAL:  return "TOKEN_INTEGER_LITERAL";
+        case TOKEN_STRING_LITERAL:   return "TOKEN_STRING_LITERAL";
+        case TOKEN_SYMBOL:           return "TOKEN_SYMBOL";
+        case TOKEN_UNKNOWN:          return "TOKEN_UNKNOWN";
+        default:                     return "UNKNOWN_TOKEN_KIND";
+    }
+}
