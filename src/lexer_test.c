df71213b988c4b6b07f50d8b6a30ee043e1077ad
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-28 16:59:37 -0400

    Create lexer_test.c
    Built lexer test driver

---
+++ src/lexer_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lexer.c"
+
+int main(void) 
+{
+    const char *input = "int main(void) { return 0; }";
+    Token tok;
+    while ((tok = next_token(&input)).kind != TOKEN_EOF) {
+        printf("[%s, \"%s\"]\n", token_kind_to_string(tok.kind), tok.lexeme);
+        free(tok.lexeme);
+    }
+    return 0;
+}
