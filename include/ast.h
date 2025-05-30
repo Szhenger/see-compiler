9459bccbb90903e32a0dc0ab95c323196ecbe0be
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-30 09:52:31 -0400

    Update ast.h
    Commented the prototypes of the abstract syntax tree

--- include/ast.h
+++ include/ast.h
@@ -1,6 +1,7 @@
 #ifndef AST_H
 #define AST_H
 
+// Define the types of abstract syntax tree (AST) tokens
 typedef enum {
     AST_FUNCTION_DEF,
     AST_RETURN_STMT,
@@ -11,6 +12,7 @@ typedef enum {
     // Add more as needed
 } ASTNodeType;
 
+// Define the structure of abstract syntax tree (AST) nodes
 typedef struct ASTNode {
     ASTNodeType type;
     struct ASTNode* left;
