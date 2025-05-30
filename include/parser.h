01ff02672cb4b9213064e2fdc4f607d4bf4ef6fe
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-30 09:56:26 -0400

    Create parser.h
    Declared the interface of the parser instances

---
+++ include/parser.h
@@ -0,0 +1,23 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "ast.h"
+#include "token.h"
+
+// Define the structure of a parser variable
+typedef struct {
+    Token* tokens;
+    int current;
+    int length;
+} Parser;
+
+// Construct a new parser instance
+Parser* init_parser(Token* tokens, int length);
+
+// Get the associated AST for parser variable
+ASTNode* parse(Parser* parser);  // Entry point
+
+// Destroy the input parser instance
+void free_parser(Parser* parser);
+
+#endif
