b7cf20ebd468172c6912aeb195a4426eb3adb571
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-05-30 09:40:55 -0400

    Update lexer.h
    Commented the instructions in lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -1,8 +1,10 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+// Include the set of tokens defined in token.c
 #include "token.h"
 
+// Returns the next token in input stream
 Token next_token(const char **input);
 
 #endif
