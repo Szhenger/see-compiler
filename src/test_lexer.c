ec881d2a100386c0a1496b28bdad3a79d45d7cd1
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-06-04 12:46:02 -0400

    Update test_lexer.c
    Fixed a bug in the interface of token.c

--- src/test_lexer.c
+++ src/test_lexer.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lexer.h"
-#include "tokens.h"
+#include "token.h"
 
 // Basic assertion helper
 void assert_token(Token t, TokenType expected_type, const char *expected_lexeme) 
