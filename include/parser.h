cb5e70b3e33c45a21169271324c48da384178073
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-06-27 14:27:40 -0400

    Update parser.h
    Reduced the complexity of unnecessarily verbose comments

--- include/parser.h
+++ include/parser.h
@@ -4,34 +4,18 @@
 #include "ast.h"
 #include "token.h"
 
-// Represents the state of the parser
+// === Parser Structure ===
 typedef struct {
     Token *tokens;  // Token stream
     int current;    // Index of current token
     int length;     // Total number of tokens
 } Parser;
 
-// Parser lifecycle
+// === Parser Lifecycle ===
 Parser *init_parser(Token *t, int *count);
 void free_parser(Parser *p);
 
-// Token inspection utilities
-Token current_token(Parser *p);
-void advance(Parser *p);
-int match(Parser *p, TokenType type, const char *lexeme);
-
-// Statement-level parsing functions
-ASTNode *parse_return(Parser *p);
-ASTNode *parse_call(Parser *p);
-ASTNode *parse_declaration(Parser *p);
-ASTNode *parse_assignment(Parser *p);
-ASTNode *parse_statement(Parser *p);
-ASTNode *parse_statement_list(Parser *p);
-
-// Function-level parsing
-ASTNode *parse_function(Parser *p);
-
-// Entry point
+// === Entry Point ===
 ASTNode *parse(Parser *p);
 
 #endif // PARSER_H
