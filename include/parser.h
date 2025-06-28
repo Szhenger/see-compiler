3af10268845ab90fbf3f7f26a8673d84da13b8ff
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-06-28 10:07:42 -0400

    Update parser.h

--- include/parser.h
+++ include/parser.h
@@ -4,19 +4,16 @@
 #include "ast.h"
 #include "token.h"
 
-// === Parser Structure ===
 typedef struct {
-    Token *tokens;  // Token stream
-    int current;    // Index of current token
-    int length;     // Total number of tokens
+    Token *tokens; 
+    int current; 
+    int length;
 } Parser;
 
-// === Parser Lifecycle ===
 Parser *init_parser(Token *t, int *count);
 void free_parser(Parser *p);
 
-// === Entry Point ===
 ASTNode *parse(Parser *p);
 
-#endif // PARSER_H
+#endif
 
