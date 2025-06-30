c8c63e2765c2d2936ecf268c23ad977f02134fb2
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-06-30 15:19:25 -0400

    Update token.h

--- include/token.h
+++ include/token.h
@@ -1,15 +1,20 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+// Enumerates the categories of supported tokens i.e. C/C++ syntax
 typedef enum {
-    TOKEN_KEYWORD,            
+    // Keywords
+    TOKEN_KEYWORD,
 
-    TOKEN_IDENTIFIER,         
+    // Function and Variable Names
+    TOKEN_IDENTIFIER,
 
+    // Literals
     TOKEN_INTEGER_LITERAL,    
     TOKEN_STRING_LITERAL,     
-    TOKEN_CHAR_LITERAL,       
+    TOKEN_CHAR_LITERAL,
 
+    // Symbols
     TOKEN_LPAREN,            
     TOKEN_RPAREN,             
     TOKEN_LBRACE,             
@@ -18,8 +23,9 @@ typedef enum {
     TOKEN_RBRACKET,           
     TOKEN_SEMICOLON,          
     TOKEN_COMMA,              
-    TOKEN_DOT,                
+    TOKEN_DOT,       
 
+    // Binary Operators
     TOKEN_ASSIGN,             
     TOKEN_PLUS,               
     TOKEN_MINUS,             
@@ -27,41 +33,48 @@ typedef enum {
     TOKEN_SLASH,              
     TOKEN_PERCENT,            
     TOKEN_INCREMENT,          
-    TOKEN_DECREMENT,          
+    TOKEN_DECREMENT,    
 
+    // Comparison Operators
     TOKEN_EQUAL,              
     TOKEN_NOT_EQUAL,          
     TOKEN_LESS,               
     TOKEN_GREATER,            
     TOKEN_LESS_EQUAL,         
-    TOKEN_GREATER_EQUAL,      
+    TOKEN_GREATER_EQUAL, 
 
+    // Logical Operators
     TOKEN_AND,                
     TOKEN_OR,                 
-    TOKEN_NOT,                
+    TOKEN_NOT, 
 
+    // Bitwise Operators
     TOKEN_BIT_AND,            
     TOKEN_BIT_OR,             
     TOKEN_BIT_XOR,            
     TOKEN_BIT_NOT,            
     TOKEN_LEFT_SHIFT,         
-    TOKEN_RIGHT_SHIFT,        
+    TOKEN_RIGHT_SHIFT,  
 
+    // Special Operators 
     TOKEN_ARROW,              
     TOKEN_QUESTION,           
-    TOKEN_COLON,              
+    TOKEN_COLON,       
 
+    // Sentient Values
     TOKEN_UNKNOWN,
     TOKEN_EOF
 } TokenCategory;
 
+// Defines the structure of a token i.e. C/C++ data and instructions
 typedef struct {
-    TokenCategory category;
-    char *lexeme;     
-    int line;               
-    int column;                
+    TokenCategory category; // Category Field
+    char *lexeme;           // Literal Field     
+    int line;               // Line Number Field (Optional)               
+    int column;             // Column Number Field (Optional) 
 } Token;
 
+// Returns the inputted token category as a string for debugging 
 const char *token_category_to_string(TokenCategory category);
 
 #endif
