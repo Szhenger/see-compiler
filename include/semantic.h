c5bc1709c4382d229cf39a067a713d40e28deffa
Shuo <115667535+Szhenger@users.noreply.github.com>
2025-07-01 11:17:45 -0400

    Update semantic.h

--- include/semantic.h
+++ include/semantic.h
@@ -3,11 +3,13 @@
 
 #include "ast.h"
 
+// Enumerate the types of semantic results
 typedef enum {
     SEMANTIC_OK = 0,
     SEMANTIC_ERROR
 } SemanticResult;
 
+// Analyze the AST for semantic errors
 SemanticResult analyze(ASTNode *root);
 
 #endif
